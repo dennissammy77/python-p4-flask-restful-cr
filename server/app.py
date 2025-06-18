c6bb49cf1ddc5c9af64dcbc9c786c257695d62de
dennissammy77 <dennissammy77@gmail.com>
2025-06-18 13:12:55 +0300

    restfult

--- server/app.py
+++ server/app.py
@@ -17,7 +17,44 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        response_dict = { "message": "Welcome to the Newsletter RESTful API", }
+        response = make_response( response_dict, 200 )
+        return response
+
+class Newsletters(Resource):
+    def get(self):
+        response_dict = [n.to_dict() for n in Newsletter.query.all()]
+        response = make_response( response_dict, 200 )
+        return response
+
+    def post(self):
+        data=request.get_json()
+        new_record=Newsletter(
+            title=data['title'],
+            body=data['body'],
+        )
+        db.session(new_record)
+        db.commit()
+
+        return make_response( new_record.to_dict(), 200 )
+
+class NewsletterByID(Resource):
+
+    def get(self, id):
+
+        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+
+        return response
+
+api.add_resource(Home, '/')
+api.add_resource(Newsletters, '/newsletters')
+api.add_resource(NewsletterByID, '/newsletters/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
